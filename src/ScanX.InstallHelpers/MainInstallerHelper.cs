4582ce360af3e89247c375e046664a49fb75fc23
Bader Albarrak <balbarak@live.com>
2018-07-14 00:59:42 +0300

    now installer work correctly

--- src/ScanX.InstallHelpers/MainInstallerHelper.cs
+++ src/ScanX.InstallHelpers/MainInstallerHelper.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Diagnostics;
 using System.Windows;
+using System.IO.Compression;
 
 namespace ScanX.InstallHelpers
 {
@@ -22,17 +23,53 @@ namespace ScanX.InstallHelpers
         public override void Install(IDictionary stateSaver)
         {
             base.Install(stateSaver);
-
-            string path = this.Context.Parameters["targetdir"];
-
-            File.Create($"{path}\\FuckOff.txt");
-
         }
+
         protected override void OnAfterInstall(IDictionary savedState)
         {
-            MessageBox.Show("Debug Me");
-
             base.OnAfterInstall(savedState);
+
+            string path = this.Context.Parameters["targetdir"];
+            string servicePath = $"{path}\\Protocol\\ScanX.Protocol.exe";
+            
+            ExtractProtocl(path);
+            
+            ServiceHelper.InstallService(servicePath);
+
+            ServiceHelper.StartService();
+        }
+
+        protected override void OnBeforeUninstall(IDictionary savedState)
+        {
+            ServiceHelper.StopService();
+            
+            base.OnBeforeUninstall(savedState);
+        }
+
+        protected override void OnAfterUninstall(IDictionary savedState)
+        {
+            base.OnAfterUninstall(savedState);
+
+            ServiceHelper.DeleteService();
+
+            string path = this.Context.Parameters["targetdir"];
+            var protocolDir = $"{path}\\Protocol";
+
+            if (Directory.Exists(protocolDir))
+                Directory.Delete(protocolDir,true);
+            
+        }
+
+        private void ExtractProtocl(string targetDir)
+        {
+            var sourcePath = $"{targetDir}\\packages\\Protocol-v1.zip";
+            var outputPath = $"{targetDir}\\Protocol";
+
+            if (!Directory.Exists(outputPath))
+                Directory.CreateDirectory(outputPath);
+
+            ZipFile.ExtractToDirectory(sourcePath,outputPath);
+
         }
     }
 }
---
+++ src/ScanX.InstallHelpers/ServiceHelper.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScanX.InstallHelpers
+{
+    public class ServiceHelper
+    {
+        private static string _serviceName = "ScanX";
+
+        public static void InstallService(string servicePath)
+        {
+            using (Process process = new Process())
+            {
+                ProcessStartInfo startupInfo = new ProcessStartInfo()
+                {
+                    FileName = "cmd.exe",
+                    Arguments = $"/C sc create {_serviceName} binPath=\"{servicePath}\" start=auto"
+                };
+
+                process.StartInfo = startupInfo;
+
+                process.Start();
+
+                process.WaitForExit();
+
+            }
+        }
+
+        public static void StartService()
+        {
+            using (Process process = new Process())
+            {
+
+                ProcessStartInfo startupInfo = new ProcessStartInfo()
+                {
+                    FileName = "cmd.exe",
+                    Arguments = $"/C sc start {_serviceName}"
+                };
+
+                process.StartInfo = startupInfo;
+
+                process.Start();
+
+                process.WaitForExit();
+            }
+        }
+
+        public static void StopService()
+        {
+            using (Process process = new Process())
+            {
+
+                ProcessStartInfo startupInfo = new ProcessStartInfo()
+                {
+                    FileName = "cmd.exe",
+                    Arguments = $"/C sc stop {_serviceName}"
+                };
+
+                process.StartInfo = startupInfo;
+
+                process.Start();
+
+                process.WaitForExit();
+
+            }
+        }
+
+        public static void DeleteService()
+        {
+            using (Process process = new Process())
+            {
+                ProcessStartInfo startupInfo = new ProcessStartInfo()
+                {
+                    FileName = "cmd.exe",
+                    Arguments = $"/c sc delete \"{_serviceName}\""
+                };
+
+                process.StartInfo = startupInfo;
+
+                process.Start();
+
+                process.WaitForExit();
+
+            }
+        }
+    }
+}
