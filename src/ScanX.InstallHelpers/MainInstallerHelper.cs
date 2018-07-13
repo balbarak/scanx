49c862a809c59cc7551e3c6981741c8fe45afee8
Bader Albarrak <balbarak@live.com>
2018-07-13 21:07:42 +0300

    installer class

---
+++ src/ScanX.InstallHelpers/MainInstallerHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace ScanX.InstallHelpers
+{
+    [RunInstaller(true)]
+    public partial class MainInstallerHelper : System.Configuration.Install.Installer
+    {
+        public MainInstallerHelper()
+        {
+            InitializeComponent();
+        }
+
+        public override void Install(IDictionary stateSaver)
+        {
+            Debugger.Launch();
+
+            base.Install(stateSaver);
+
+            string path = this.Context.Parameters["targetdir"];
+
+            File.Create($"{path}\\FuckOff.txt");
+
+        }
+        protected override void OnAfterInstall(IDictionary savedState)
+        {
+            
+            base.OnAfterInstall(savedState);
+        }
+    }
+}
