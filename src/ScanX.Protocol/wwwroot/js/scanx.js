dbbf854f6672bdf7e1b08e98056f90ecedbe00dc
Bader Albarrak <balbarak@live.com>
2018-07-20 01:14:31 +0300

    add signalr js

--- src/ScanX.Protocol/wwwroot/js/scanx.js
+++ src/ScanX.Protocol/wwwroot/js/scanx.js
@@ -1,1 +1,11 @@
 ﻿const url = "";
+
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("http://localhost:61234/scanx")
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+
+$(function () {
+    connection.start().catch(err => console.error(err.toString()));
+})
