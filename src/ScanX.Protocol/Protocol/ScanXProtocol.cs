6ad448d024a1371d7f35489e3ff08a4ecee904dc
Bader Albarrak <balbarak@live.com>
2018-07-21 01:36:03 +0300

    update

--- src/ScanX.Protocol/Protocol/ScanXProtocol.cs
+++ src/ScanX.Protocol/Protocol/ScanXProtocol.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,6 +17,17 @@ namespace ScanX.Protocol.Protocol
         public override Task OnDisconnectedAsync(Exception exception)
         {
             return base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task Scan()
+        {
+            var data = File.ReadAllBytes("wwwroot\\images\\1.png");
+
+            var imageBase64 = Convert.ToBase64String(data);
+
+            await Clients.Caller.SendAsync("ImageScanned", imageBase64);
+
+            await Task.CompletedTask;
         }
     }
 }
--- src/ScanX.Protocol/wwwroot/js/scanx.js
+++ src/ScanX.Protocol/wwwroot/js/scanx.js
@@ -1,11 +1,26 @@
-﻿const url = "";
-
-const connection = new signalR.HubConnectionBuilder()
-    .withUrl("http://localhost:61234/scanx")
-    .configureLogging(signalR.LogLevel.Information)
-    .build();
-
-
+﻿
 $(function () {
-    connection.start().catch(err => console.error(err.toString()));
+    
 })
+
+
+class ScanX {
+
+    constructor() {
+
+        this.connection = new signalR.HubConnectionBuilder()
+            .withUrl("http://localhost:61234/scanx")
+            .configureLogging(signalR.LogLevel.Information)
+            .build();
+    }
+    
+    connect() {
+
+        this.connection.start().catch(err => console.error(err.toString()));
+    }
+
+    scan() {
+
+        this.connection.invoke("Scan").catch(err => console.error(err.toString()));
+    }
+}
