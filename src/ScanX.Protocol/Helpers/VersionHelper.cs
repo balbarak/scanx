3a981e25d5eb6cab32fd6989c415a5b770ffed96
balbarak <balbarak@hotmail.com>
2021-02-25 17:56:22 +0300

    add version

---
+++ src/ScanX.Protocol/Helpers/VersionHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScanX.Protocol.Helpers
+{
+    public class VersionHelper
+    {
+        public static string Version => "1.7.0";
+    }
+}
